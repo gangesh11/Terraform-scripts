f08cbe9ed057615549b6228ad97ce07db2d1a9ea
gangesh11 <gangeshvats11@yahoo.co.in>
2018-07-11 18:42:34 +0530

    fixed the code bug

--- vpc.tf
+++ vpc.tf
@@ -43,6 +43,30 @@ resource "aws_internet_gateway" "main-gw" {
   }
 }
 
+## Establishes a relationship resource between the "My-VPC-03-Scanner" and "My-VPC-01" VPC.
+
+resource "aws_vpc_peering_connection" "My-VPC-03-Scanner2My-VPC-01" {
+  peer_vpc_id = "${aws_vpc.My-VPC-01.id}"
+  vpc_id      = "${aws_vpc.My-VPC-03-Scanner.id}"
+  auto_accept = true
+}
+
+## Establishes a relationship resource between the "My-VPC-03-Scanner" and "My-VPC-02" VPC.
+
+resource "aws_vpc_peering_connection" "My-VPC-03-Scanner2My-VPC-02" {
+  peer_vpc_id = "${aws_vpc.My-VPC-02.id}"
+  vpc_id      = "${aws_vpc.My-VPC-03-Scanner.id}"
+  auto_accept = true
+}
+
+## Establishes a relationship resource between the "My-VPC-03-Scanner" and "Public-VPC-04" VPC.
+
+resource "aws_vpc_peering_connection" "My-VPC-03-Scanner2Public-VPC-04" {
+  peer_vpc_id = "${aws_vpc.Public-VPC-04.id}"
+  vpc_id      = "${aws_vpc.My-VPC-03-Scanner.id}"
+  auto_accept = true
+}
+
 # route tables
 resource "aws_route_table" "main-public-01" {
   vpc_id = "${aws_vpc.My-VPC-03-Scanner.id}"
@@ -50,6 +74,21 @@ resource "aws_route_table" "main-public-01" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.main-gw.id}"
+  }
+
+  route {
+    cidr_block                = "${aws_vpc.My-VPC-01.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-01.id}"
+  }
+
+  route {
+    cidr_block                = "${aws_vpc.My-VPC-02.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-02.id}"
+  }
+
+  route {
+    cidr_block                = "${aws_vpc.Public-VPC-04.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2Public-VPC-04.id}"
   }
 
   tags {
@@ -109,13 +148,18 @@ resource "aws_subnet" "Private-Subnet-2-VPC01" {
 resource "aws_route_table" "vpc01-route" {
   vpc_id = "${aws_vpc.My-VPC-01.id}"
 
+  route {
+    cidr_block                = "${aws_vpc.My-VPC-03-Scanner.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-01.id}"
+  }
+
   tags {
     Name = "vpc01-route"
   }
 }
 
 # route associations private
-  resource "aws_route_table_association" "Private-Subnet-1-VPC01" {
+resource "aws_route_table_association" "Private-Subnet-1-VPC01" {
   subnet_id      = "${aws_subnet.Private-Subnet-1-VPC01.id}"
   route_table_id = "${aws_route_table.vpc01-route.id}"
 }
@@ -166,13 +210,18 @@ resource "aws_subnet" "Private-Subnet-2-VPC02" {
 resource "aws_route_table" "vpc02-route" {
   vpc_id = "${aws_vpc.My-VPC-02.id}"
 
+  route {
+    cidr_block                = "${aws_vpc.My-VPC-03-Scanner.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-02.id}"
+  }
+
   tags {
     Name = "vpc02-route"
   }
 }
 
 # route associations private
-  resource "aws_route_table_association" "Private-Subnet-1-VPC02" {
+resource "aws_route_table_association" "Private-Subnet-1-VPC02" {
   subnet_id      = "${aws_subnet.Private-Subnet-1-VPC02.id}"
   route_table_id = "${aws_route_table.vpc02-route.id}"
 }
@@ -181,6 +230,7 @@ resource "aws_route_table_association" "Private-Subnet-2-VPC02" {
   subnet_id      = "${aws_subnet.Private-Subnet-2-VPC02.id}"
   route_table_id = "${aws_route_table.vpc02-route.id}"
 }
+
 ##########################################Public-VPC-04################################
 # Public-VPC-04
 resource "aws_vpc" "Public-VPC-04" {
@@ -236,6 +286,11 @@ resource "aws_route_table" "Public-VPC-04" {
     gateway_id = "${aws_internet_gateway.main-gw-vpc04.id}"
   }
 
+  route {
+    cidr_block                = "${aws_vpc.My-VPC-03-Scanner.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2Public-VPC-04.id}"
+  }
+
   tags {
     Name = "Public-VPC-04"
   }
@@ -251,70 +306,3 @@ resource "aws_route_table_association" "Public-Subnet-2-VPC04" {
   subnet_id      = "${aws_subnet.Public-Subnet-2-VPC04.id}"
   route_table_id = "${aws_route_table.Public-VPC-04.id}"
 }
-
-
-##########################################VPC Peering##################################
-
-data "aws_caller_identity" "current" {}
-
-## VPC peering connection.
-
-## Establishes a relationship resource between the "My-VPC-03-Scanner" and "My-VPC-01" VPC.
-
-resource "aws_vpc_peering_connection" "My-VPC-03-Scanner2My-VPC-01" {
-  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
-  peer_vpc_id   = "${aws_vpc.My-VPC-01.id}"
-  vpc_id        = "${aws_vpc.My-VPC-03-Scanner.id}"
-  auto_accept   = true
-}
-
-##  Route rule.
-
-##  Creates a new route rule on the "My-VPC-03-Scanner" VPC main route table. All requests to the "My-VPC-01" VPC's IP range will be directed to the VPC peering connection.
-
-resource "aws_route" "My-VPC-03-Scanner2My-VPC-01"{
-  route_table_id            = "${aws_route_table.main-public-01.id}"
-  destination_cidr_block    = "${aws_vpc.My-VPC-01.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-01.id}"
-}
-
-
-##  Route rule.
-
-##  Creates a new route rule on the "My-VPC-01" VPC main route table. All requests to the "My-VPC-03-Scanner" VPC's IP range will be directed to the VPC peering connection.
-
-resource "aws_route" "My-VPC-012My-VPC-03-Scanner" {
-  route_table_id            = "${aws_route_table.vpc01-route.id}"
-  destination_cidr_block    = "${aws_vpc.My-VPC-03-Scanner.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-01.id}"
-}
-
-## Establishes a relationship resource between the "My-VPC-03-Scanner" and "My-VPC-02" VPC.
-
-resource "aws_vpc_peering_connection" "My-VPC-03-Scanner2My-VPC-02" {
-  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
-  peer_vpc_id   = "${aws_vpc.My-VPC-02.id}"
-  vpc_id        = "${aws_vpc.My-VPC-03-Scanner.id}"
-  auto_accept   = true
-}
-
-##  Route rule.
-
-##  Creates a new route rule on the "My-VPC-03-Scanner" VPC main route table. All requests to the "My-VPC-02" VPC's IP range will be directed to the VPC peeringconnection.
-
-resource "aws_route" "My-VPC-03-Scanner2My-VPC-02-01" {
-  route_table_id            = "${aws_route_table.main-public-01.id}"
-  destination_cidr_block    = "${aws_vpc.My-VPC-02.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-02.id}"
-}
-
-##  Route rule.
-
-##  Creates a new route rule on the "My-VPC-02" VPC main route table. All requests to the "My-VPC-01" VPC's IP range will be directed to the VPC peering connection.
-
-resource "aws_route" "My-VPC-02My-VPC-03-Scanner" {
-  route_table_id            = "${aws_route_table.vpc02-route.id}"
-  destination_cidr_block    = "${aws_vpc.My-VPC-03-Scanner.cidr_block}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.My-VPC-03-Scanner2My-VPC-02.id}"
-}
-
